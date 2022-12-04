01445b52d5df427af872aef70bda037a0917191f
Kushal Patel <kxp988@student.bham.ac.uk>
2022-12-04 22:59:35 +0000

    solutions for day 1

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -3,19 +3,18 @@ with open('day1-input.txt') as f:
     highestCalorieTotal = -1
     calorieTotal = 0
     lines = f.readlines()
-
+ 
     # iterate through each line in the input file
     for line in lines:
         # if the line is blank, it will only contain the newline character which has ASCII value 10
         if ((len(line) == 1) and (ord(line) == 10)):
-            # check if the sum of calories of the items which this elf is carrying is greater 
-            # than the current highest sum of calories found
-            if calorieTotal > highestCalorieTotal:
-                highestCalorieTotal = calorieTotal
+            # as this is the end of an elf's items, store the sum of the calories of the items which this elf is carrying
+            calorieTotals.append(calorieTotal)
             calorieTotal = 0
         else:
             # this line represents the calorie count of an item
             # increment calorieTotal by the number of calories which this item contains
             calorieTotal += int(line)
-    # return the highest calorie count found
-    print(highestCalorieTotal)
+    # sort the list of total calories which each elf is carrying in ascending order and
+    # return the sum of the last three elements of this list 
+    print(sum(sorted(calorieTotals)[-3:]))
