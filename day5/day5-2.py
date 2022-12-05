37fb004a94ffe4314d5d3a916bc5d0e7c0ef1bb4
Kushal Patel <kxp988@student.bham.ac.uk>
2022-12-05 16:19:28 +0000

    solutions for day 5

---
+++ day5/day5-2.py
@@ -0,0 +1,46 @@
+import re
+
+with open('day5-input.txt') as f:
+    lines = f.readlines()
+    numStacks = 0
+    lineBreakIndex = 0
+    # find the number of supply stacks
+    for i in range(len(lines)):
+        # if the line is blank, it will only contain the newline character which has ASCII value 10
+        if ((len(lines[i]) == 1) and (ord(lines[i]) == 10)):
+            numStacks = max(list(map(int,(lines[i - 1].split("   ")))))
+            lineBreakIndex = i
+            break
+    
+    # instantiate a list of stacks, whose length equals the number of supply stacks
+    stackList = []
+    for i in range(numStacks):
+        stack = []
+        stackList.append(stack)
+
+    # load the state of the supply stacks into the stack objects
+    for i in range(lineBreakIndex, -1, -1):
+        line = lines[i]
+        for i in range(len(line)):
+            if (ord("A") <= ord(line[i]) <= ord("Z")):
+                stackList[i // 4].append(line[i])
+
+    # conduct moves
+    for i in range(lineBreakIndex, len(lines)):
+        # parse line to find how many crates we are moving and their source + destination
+        line = lines[i]
+        numsFound = re.findall("\d+", line)
+        if (len(numsFound) > 0):
+            moveNo = int(numsFound[0])
+            moveFrom = int(numsFound[1]) - 1
+            moveTo = int(numsFound[2]) - 1
+            # move crates
+            takenFrom = stackList[moveFrom][-moveNo:]
+            del stackList[moveFrom][-moveNo:]
+            stackList[moveTo].extend(takenFrom)
+
+    # print crate on top of each stack
+    outString = ""
+    for stack in stackList:
+        outString += stack.pop()
+    print(outString)
